d2170dedc3e396456a58852c779fbf019447960c
faris shomou <fshomou@hotmail.com>
2018-03-04 12:33:56 -0500

    Test for Add Blog and Psot

--- EFGetStarted.RestAPI.ExistingDb.Tests/BlogUowControllerTests.cs
+++ EFGetStarted.RestAPI.ExistingDb.Tests/BlogUowControllerTests.cs
@@ -13,7 +13,7 @@ namespace EFGetStarted.RestAPI.ExistingDb.Tests
     public class BlogUowControllerTests
     {
         [TestMethod]
-        public async Task Add_writes_to_database()
+        public async Task Get_Blogs()
         {
           
 
@@ -62,6 +62,123 @@ namespace EFGetStarted.RestAPI.ExistingDb.Tests
             }
         }
 
+        [TestMethod]
+        public async Task Add_Blog()
+        {
 
+
+            try
+            {
+                string connection = @"Server=(localdb)\mssqllocaldb;Database=BloggingGeneric;Trusted_Connection=True;ConnectRetryCount=0";
+                var options = new DbContextOptionsBuilder<DataContext>()
+               .UseSqlServer(connection)
+               .Options;
+
+
+                //// Create the schema in the database
+                //using (var context = new BloggingContext(options))
+                //{
+                //    context.Database.EnsureCreated();
+                //}
+
+
+                // Run the test against one instance of the context
+                using (var context = new DataContext(options))
+                {
+                    //IBlogRepositoryGeneric blogsRepository = new BlogRepositoryGenerics(context);
+
+                    var uow = new UnitOfWork<DataContext>(context);
+                    var repo = uow.GetRepository<Blog>();
+                    var blogsUOWController = new BlogsUOWController(uow);
+                    Blog blog = new Blog();
+                    blog.Url = "test from VS";
+
+                    repo.Add(blog);
+                    uow.SaveChanges();
+
+                                    }
+
+                //Use a separate instance of the context to verify correct data was saved to database
+                //using (var context = new BloggingContext(options))
+                //{
+                //    Assert.AreEqual(1, context.Blogs.Count());
+                //    Assert.AreEqual("http://sample.com", context.Blogs.Single().Url);
+                //}
+            }
+            catch (Exception e)
+            {
+
+
+            }
+
+            finally
+            {
+
+            }
+        }
+
+        [TestMethod]
+        public async Task Add_Blog_And_Post()
+        {
+
+
+            try
+            {
+                string connection = @"Server=(localdb)\mssqllocaldb;Database=BloggingGeneric;Trusted_Connection=True;ConnectRetryCount=0";
+                var options = new DbContextOptionsBuilder<DataContext>()
+               .UseSqlServer(connection)
+               .Options;
+
+
+                //// Create the schema in the database
+                //using (var context = new BloggingContext(options))
+                //{
+                //    context.Database.EnsureCreated();
+                //}
+
+
+                // Run the test against one instance of the context
+                using (var context = new DataContext(options))
+                {
+                    //IBlogRepositoryGeneric blogsRepository = new BlogRepositoryGenerics(context);
+
+                    var uow = new UnitOfWork<DataContext>(context);
+                    var repo = uow.GetRepository<Blog>();
+                    var blogsUOWController = new BlogsUOWController(uow);
+                    Blog blog = new Blog();
+                    blog.Url = "test from VS lbog and post";
+
+                    Post post = new Post();
+                    post.Blog = blog;
+                    post.Content = "sdfdsfdsfdsfs";
+                    post.Title = "tititel";
+                    
+
+                    blog.Post.Add(post);
+
+                    repo.Add(blog);
+                    uow.SaveChanges();
+
+                  
+                }
+
+                //Use a separate instance of the context to verify correct data was saved to database
+                //using (var context = new BloggingContext(options))
+                //{
+                //    Assert.AreEqual(1, context.Blogs.Count());
+                //    Assert.AreEqual("http://sample.com", context.Blogs.Single().Url);
+                //}
+            }
+            catch (Exception e)
+            {
+
+
+            }
+
+            finally
+            {
+
+            }
+        }
     }
 }
